51695d270e6e711acd246f7656192ebcc14c1d2b
syaofox <syaofox@gmail.com>
2025-11-09 17:40:58 +0800

    Refactor flake.nix to improve package set generation and system configuration. Introduced mkPkgSet function for better organization and clarity in defining packages (dwm, slstatus, slock) and their dependencies. Updated nixosConfigurations to utilize the new structure.

--- flake.nix
+++ flake.nix
@@ -12,58 +12,75 @@
   };
 
   outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    let
+      mkPkgSet =
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-        # 直接编译本地源码
-        dwm = pkgs.stdenv.mkDerivation {
-          pname = "dwm";
-          version = "6.4";
-          src = ./src/dwm;
-          buildInputs = with pkgs; [ libX11 libXinerama libXft ];
-          makeFlags = [ "PREFIX=$(out)" ];
+          # 直接编译本地源码
+          dwm = pkgs.stdenv.mkDerivation {
+            pname = "dwm";
+            version = "6.4";
+            src = ./src/dwm;
+            buildInputs = with pkgs; [ libX11 libXinerama libXft ];
+            makeFlags = [ "PREFIX=$(out)" ];
+          };
+
+          slstatus = pkgs.stdenv.mkDerivation {
+            pname = "slstatus";
+            version = "1";
+            src = ./src/slstatus;
+            buildInputs = with pkgs; [ libX11 ];
+            makeFlags = [ "PREFIX=$(out)" ];
+          };
+
+          slock = pkgs.stdenv.mkDerivation {
+            pname = "slock";
+            version = "1.5";
+            src = ./src/slock;
+            buildInputs = with pkgs; [ libX11 libXinerama libXft ];
+            makeFlags = [ "PREFIX=$(out)" ];
+          };
+        in {
+          inherit pkgs dwm slstatus slock;
         };
 
-        slstatus = pkgs.stdenv.mkDerivation {
-          pname = "slstatus";
-          version = "1";
-          src = ./src/slstatus;
-          buildInputs = with pkgs; [ libX11 ];
-          makeFlags = [ "PREFIX=$(out)" ];
-        };
+      perSystem =
+        flake-utils.lib.eachDefaultSystem (
+          system:
+          let
+            pkgSet = mkPkgSet system;
+          in {
+            packages = {
+              inherit (pkgSet) dwm slstatus slock;
+            };
+          }
+        );
 
-        slock = pkgs.stdenv.mkDerivation {
-          pname = "slock";
-          version = "1.5";
-          src = ./src/slock;
-          buildInputs = with pkgs; [ libX11 libXinerama libXft ];
-          makeFlags = [ "PREFIX=$(out)" ];
-        };
+      nixSystem = "x86_64-linux";
+      nixPkgSet = mkPkgSet nixSystem;
+    in
+    perSystem // {
+      nixosConfigurations.nix3060 = nixpkgs.lib.nixosSystem {
+        system = nixSystem;
+        modules = [
+          /etc/nixos/hardware-configuration.nix
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.syaofox = import ./home.nix {
+              inherit (nixPkgSet) pkgs dwm slstatus slock;
+            };
+          }
+        ];
+      };
 
-      in {
-        packages = { inherit dwm slstatus slock; };
-
-        nixosConfigurations.nix3060 = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            /etc/nixos/hardware-configuration.nix
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.syaofox = import ./home.nix {
-                inherit pkgs dwm slstatus slock;
-              };
-            }
-          ];
-        };
-
-        templates.default = {
-          path = ./.;
-          description = "syaofox DWM NixOS rice (startx, no DM)";
-        };
-      }
-    );
+      templates.default = {
+        path = ./.;
+        description = "syaofox DWM NixOS rice (startx, no DM)";
+      };
+    };
 }
